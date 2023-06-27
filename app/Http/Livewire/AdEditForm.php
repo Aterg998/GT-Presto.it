c161de5e1fe428d1be7283986d6637805d0c4984
Emanuele Manera <emanuele.manera01@gmail.com>
2023-06-27 20:26:33 +0200

    componenti livewire annunci

---
+++ app/Http/Livewire/AdEditForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AdEditForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.ad-edit-form');
+    }
+}
---
+++ resources/views/ads/index.blade.php
@@ -0,0 +1,1 @@
+<livewire:ad-index-list />

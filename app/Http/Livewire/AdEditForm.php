c484627f86dd2ff65c264047fcf4b2b0f07236ae
Emanuele Manera <emanuele.manera01@gmail.com>
2023-06-27 21:27:48 +0200

    componenti ad index edit

--- app/Http/Livewire/AdEditForm.php
+++ app/Http/Livewire/AdEditForm.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Ad;
 use Livewire\Component;
 
 class AdEditForm extends Component
 {
+    public $title, $price, $description, $image; 
+
+    public Ad $ad;
+
+    protected $rules = [
+        'title' => 'required|min:10',
+        'price' => 'required',
+        'description' => 'required|min:10|max:500',
+    ];
+
+    public function mount()
+    {
+        $this->title = $this->ad->title;
+        $this->price = $this->ad->price;
+        $this->description = $this->ad->description;
+        $this->image = $this->ad->image;
+    }
+
+    public function updated($propertyName){
+        $this->validateOnly($propertyName);
+    }
+
+
+    public function update()
+    {
+        
+        $this->validate();
+
+        $this->ad->update([
+            'title' => $this->title,
+            'price' => $this->price,
+            'description' => $this->description,
+            'image' => $this->image,
+        ]);
+
+        session()->flash('success', 'Annuncio modificato con successo!');
+    }
+
+
     public function render()
     {
         return view('livewire.ad-edit-form');

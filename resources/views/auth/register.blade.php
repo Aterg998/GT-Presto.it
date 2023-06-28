5666a8f9143aa7a4bcb16702e8ed97764854ef03
Alberto di Clemente <alberto.diclemente@gmail.com>
2023-06-28 20:55:21 +0200

    views login\register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,51 @@
+<x-main> 
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+                <div class="d-table-cell align-middle">
+
+                    <div class="text-center mt-4">
+                        <h1 class="h2">Registrati</h1>
+                       
+                    </div>
+
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="m-sm-4">
+                                <div class="text-center">
+                                    <img src="https://images.pexels.com/photos/9496721/pexels-photo-9496721.jpeg" alt="Andrew Jones" class="img-fluid rounded-circle" width="132" height="132">
+                                </div>
+                                <form method="post" action={{route('register')}}>
+                                    @csrf
+                                    @method('POST')
+                                    <div class="form-group">
+                                        <label>Username</label>
+                                        <input class="form-control form-control-lg" type="text" name="name" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input class="form-control form-control-lg" type="email" name="email" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input class="form-control form-control-lg" type="password" name="password" >
+                                       
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label>Conferma Password</label>
+                                        <input class="form-control form-control-lg" type="password" name="password_confirmation" >
+                                    <div class="text-center mt-3">
+                                      
+                                        <button type="submit" class="btn btn-lg btn-primary">Accedi</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>   
+</x-main>

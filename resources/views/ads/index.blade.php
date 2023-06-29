30bbc9e59f8292868de0234d1f65ea078a5b39e9
Alberto di Clemente <alberto.diclemente@gmail.com>
2023-06-29 20:27:38 +0200

    Index provvisorio

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -1,1 +1,24 @@
-<livewire:ad-index-list />
+<x-main>
+
+
+    <div class="section-header">
+        <livewire:ad-index-list />
+        <h2>Articoli</h2>
+        <p>non so cosa scrivere</p>
+      </div>
+
+      @foreach ($ads as $ad )
+          
+    
+      <div class="col-xl-4 col-md-6 portfolio-item filter-product">
+        <div class="portfolio-wrap">
+          <a href="assets/img/portfolio/product-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-1.jpg" class="img-fluid" alt=""></a>
+          <div class="portfolio-info">
+            <h4><a href="{{route('ads.show')}}" title="title">{{$ad->title}}</a></h4>
+            <p>Prezzo {{$ad->price}}</p>
+            <p>{{$ad->description}}</p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+</x-main>

77d80c6a796b010a3bd6670f6c357d6f1565a72d
Aterg998 <greta.tunesi@forestvalley.org>
2023-07-10 21:20:29 +0200

    iniziata user story 5

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -32,6 +32,10 @@ class Ad extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
+
     public function setAccepted($value) {
         $this->is_accepted = $value;
         $this->save();

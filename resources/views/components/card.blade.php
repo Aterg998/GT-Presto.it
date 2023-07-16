187d4950dd40d29995212a23f430112b68ff7ed5
Aterg998 <greta.tunesi@forestvalley.org>
2023-07-16 17:43:57 +0200

    label categorie

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,7 @@
 <div class="gy-4 portfolio-container m-5">
   <div class="portfolio-item filter-app">
     <div class="portfolio-wrap">
+      <a class="btn btn-presto btn-sm text-violet position-absolute mt-3 ms-3 z-20 "></span> {{$ad->category->name}}</a>
       <a href="{{route('ads.show', $ad->id)}}" data-gallery="portfolio-gallery-app" class="glightbox">
         <img src="
         @if (!$ad->images()->get()->isEmpty())
@@ -10,9 +11,11 @@
         @endif" class="img-fluid" alt="">
       </a>
       <div class="portfolio-info">
-        <h4><a href="{{route('ads.show', $ad->id)}}" title="title" class="accenti2 fw-medium">{{$ad->title}}</a></h4>
+        <h4 class="mt-3">
+          <a href="{{route('ads.show', $ad->id)}}" title="title" class="accenti2">{{$ad->title}}
+          </a>
+        </h4>
         <p class="text-black" style="line-height: 1em"> <span class="accenti">{{__('messages.price')}}: </span> € {{$ad->price}}</p>
-        <p class="text-black" style="line-height: 1.4em"><span class="accenti">{{__('messages.category')}}:</span> {{$ad->category->name}}</p>
         <p class="text-black" style="line-height: 1em"><span class="accenti">{{__('messages.description')}}: </span>{{$ad->description}}</p>
       </div>
     </div>
